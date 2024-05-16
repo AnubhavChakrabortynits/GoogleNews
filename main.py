3f82ebae4e863ffc94fbdbd473edd7f468b611a3
Anubhav <chakrabortyanubhav0@gmail.com>
2024-05-16 22:41:49 +0530

    error fix

--- main.py
+++ main.py
@@ -6,5 +6,3 @@ from views import views
 app = Flask(__name__)
 app.register_blueprint(views,url_prefix='/views')
 app.config['MAX_CONTENT_LENGTH'] = 128 * 1024 * 1024
-if __name__ == '__main__':
-    app.run(debug=False)
