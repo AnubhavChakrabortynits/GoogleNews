a47aeeb0cff85b5dff57a51f31bb9261f69974a0
Anubhav <chakrabortyanubhav0@gmail.com>
2024-05-16 23:14:37 +0530

    nltk downloads

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from utils import setup_nltk
 from views import views
 import nltk
 nltk.download('punkt')
+nltk.download('stopwords')
+
 app = Flask(__name__)
 app.register_blueprint(views,url_prefix='/views')
 app.config['MAX_CONTENT_LENGTH'] = 128 * 1024 * 1024
