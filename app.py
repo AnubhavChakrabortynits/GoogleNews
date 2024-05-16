6608df05170999c2b6a37f31a75c5714ebde6aa9
Anubhav <chakrabortyanubhav0@gmail.com>
2024-05-16 23:09:09 +0530

    change main to app

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask import render_template
 from utils import setup_nltk
 from views import views
-
+import nltk
+nltk.download('punkt')
 app = Flask(__name__)
 app.register_blueprint(views,url_prefix='/views')
 app.config['MAX_CONTENT_LENGTH'] = 128 * 1024 * 1024
